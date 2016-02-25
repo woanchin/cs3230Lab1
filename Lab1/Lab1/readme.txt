5fe3694be6ee19ef4be431a25d45746c26c66a9f
Woan Chin <woanchin.tee@gmail.com>
2016-02-25 18:38:03 +0800

    reorganised and shifted files for easy handling in future

---
+++ Lab1/Lab1/readme.txt
@@ -0,0 +1,6 @@
+A0129650
+Tee Woan Chin
+
+I have modified the display() to fit in my drawings. I made a function for each building structure.
+Coolest thing about it is that my Ferris Wheel rotates. :D
+
---
+++ Lab2/main.cpp
@@ -0,0 +1,167 @@
+// CS3241Lab2.cpp : Defines the entry point for the console application.
+#include <cmath>
+#include <iostream>
+
+/* Include header files depending on platform */
+#ifdef _WIN32
+	#include "glut.h"
+	#define M_PI 3.14159
+#elif __APPLE__
+	#include <OpenGL/gl.h>
+	#include <GLUT/GLUT.h>
+#endif
+
+using namespace std;
+
+#define numStars 100
+#define numPlanets 9
+
+class planet
+{
+public:
+	float distFromRef;
+	float angularSpeed;
+	GLfloat color[3];
+	float size;
+	float angle;
+
+	planet()
+	{
+		distFromRef = 0;
+		angularSpeed = 0;
+		color[0] = color[1] = color[2] = 0;
+		size = 0;
+		angle = 0;
+	}
+};
+
+float alpha = 0.0, k=1;
+float tx = 0.0, ty=0.0;
+planet planetList[numPlanets];
+
+void reshape (int w, int h)
+{
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-10, 10, -10, 10, -10, 10);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void init(void)
+{
+	glClearColor (0.0, 0.0, 0.3, 1.0);
+	glShadeModel (GL_SMOOTH);
+	glEnable(GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glPushMatrix();
+
+	//controls transformation
+	glScalef(k, k, k);	
+	glTranslatef(tx, ty, 0);	
+	glRotatef(alpha, 0, 0, 1);
+
+	//draw stuff here!
+
+	glPopMatrix();
+	glFlush ();
+}
+
+void idle()
+{
+	//update animation here
+	
+	glutPostRedisplay();	//after updating, draw the screen again
+}
+
+void keyboard (unsigned char key, int x, int y)
+{
+	//keys to control scaling - k
+	//keys to control rotation - alpha
+	//keys to control translation - tx, ty
+	switch (key) {
+
+		case 'a':
+			alpha+=10;
+			glutPostRedisplay();
+		break;
+
+		case 'd':
+			alpha-=10;
+			glutPostRedisplay();
+		break;
+
+		case 'q':
+			k+=0.1;
+			glutPostRedisplay();
+		break;
+
+		case 'e':
+			if(k>0.1)
+				k-=0.1;
+			glutPostRedisplay();
+		break;
+
+		case 'z':
+			tx-=0.1;
+			glutPostRedisplay();
+		break;
+
+		case 'c':
+			tx+=0.1;
+			glutPostRedisplay();
+		break;
+
+		case 's':
+			ty-=0.1;
+			glutPostRedisplay();
+		break;
+
+		case 'w':
+			ty+=0.1;
+			glutPostRedisplay();
+		break;
+		
+		case 27:
+			exit(0);
+		break;
+
+		default:
+		break;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	cout<<"CS3241 Lab 2\n\n";
+	cout<<"+++++CONTROL BUTTONS+++++++\n\n";
+	cout<<"Scale Up/Down: Q/E\n";
+	cout<<"Rotate Clockwise/Counter-clockwise: A/D\n";
+	cout<<"Move Up/Down: W/S\n";
+	cout<<"Move Left/Right: Z/C\n";
+	cout <<"ESC: Quit\n";
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize (600, 600);
+	glutInitWindowPosition (50, 50);
+	glutCreateWindow (argv[0]);
+	init ();
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);	
+	//glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+
+	return 0;
+}
